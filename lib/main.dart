df0a97f172db7770e5344121a3daea64c2bbf3aa
siushunking <siushunkingzz@gmail.com>
2022-11-06 00:50:31 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  HomePage()
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Photo Show")),
+      body: 
+      SingleChildScrollView(
+        child: Column(
+        children: [
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: Image.network("https://docs.flutter.dev/assets/images/dash/dash-fainting.gif")
+          ),
+          Container(
+            width: size.width,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(border: Border.all(width: 8.0)),
+            child: Image.network("https://docs.flutter.dev/assets/images/dash/dash-fainting.gif"),
+          )
+
+      ],),)
+      
+    );
+
+    // return ElevatedButton(onPressed: (){
+    //   setState(() {
+    //     _index += 1;
+    //   });
+    //   print(_index);
+    // }, child: Text("data"));
+
+  }
+}
---
+++ lib/page2.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Page2 extends StatelessWidget {
+  final String title;
+
+  Page2(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text(title)),);
+  }
+}
